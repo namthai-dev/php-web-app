4c52b656aa23472203d3d11103324ebbb26936f9
Nam Thai <nam.thai.dev@outlook.com>
2023-05-06 22:29:03 +0700

    Display public items

--- index.php
+++ index.php
@@ -9,4 +9,28 @@
         <a href="login.php"> Click here to login 
         <a href="register.php"> Click here to register 
     </body>
+	<br/>
+	<h2 align="center">List</h2>
+	<table width="100%" border="1px">
+			<tr>
+				<th>Id</th>
+				<th>Details</th>
+				<th>Post Time</th>
+				<th>Edit Time</th>
+			</tr>
+			<?php
+				mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+                $mysqli = mysqli_connect("localhost", "root", "", "php-web-app-db");
+				$query = mysqli_query($mysqli, "Select * from list Where public='yes'"); // SQL Query
+				while($row = mysqli_fetch_array($query))
+				{
+					Print "<tr>";
+						Print '<td align="center">'. $row['id'] . "</td>";
+						Print '<td align="center">'. $row['details'] . "</td>";
+						Print '<td align="center">'. $row['date_posted']. " - ". $row['time_posted']."</td>";
+						Print '<td align="center">'. $row['date_edited']. " - ". $row['time_edited']. "</td>";
+					Print "</tr>";
+				}
+			?>
+	</table>    
 </html>
